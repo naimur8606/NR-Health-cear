48bc55bab7f9cccdab8f8658c9e46b3e475aea21
naimur8606 <119111473+naimur8606@users.noreply.github.com>
2023-03-17 16:06:27 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function menu(){
+    const manuBtn=document.getElementById("memu-itms");
+     if(manuBtn.style.display != "block") {
+        manuBtn.style.display = "block";
+    }
+    else {
+        manuBtn.style.display = "none";
+    } 
+}
